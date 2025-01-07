a31a4ba0effdc26409a6fd1cf7122902a69a597e
praharshini16 <praharshinikamuni0216@gmail.com>
2025-01-07 10:14:09 +0530

    unary operator

---
+++ Demo014.java
@@ -0,0 +1,20 @@
+public class Demo014   
+{  
+public static void main(String args[])  
+{  
+int i = 10;  
+i++;  
+  
+System.out.println(i);  
+++i;                 
+
+System.out.println(i);  
+
+System.out.println(++i);  
+
+System.out.println(i++);  
+
+System.out.println(i);  
+}  
+}  
+    
